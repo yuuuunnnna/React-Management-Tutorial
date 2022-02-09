5257958ae4e2feef59bbafbea5be825a1cb91c17
yyyuna <73158757+yuuuunnnna@users.noreply.github.com>
2022-02-09 17:46:10 +0900

    progress bar add

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import {withStyles} from '@material-ui/core/styles';
 
 const styles = theme => ({
@@ -17,17 +18,23 @@ const styles = theme => ({
   },
   table: {
     minWidth: 1080
+  },
+  progress:{
+    margin: theme.spacing.unit * 2
   }
-})
+});
 
 
 class App extends Component {
 
   state = {
-    customers: ""
+    customers: "",
+    completed: 0
+
   }
 
   componentDidMount() {
+    this.timer = setInterval(this.progress, 20); //0.02초 마다 반복되게 설정
     this.callApi()
       .then(res => this.setState({customers: res}))
       .catch(err => console.log(err));
@@ -37,6 +44,12 @@ class App extends Component {
     const response = await fetch('/api/customers');
     const body = await response.json();
     return body;
+  }
+
+
+  progress = () => {
+    const {completed} = this.state;
+    this.setState({completed: completed >= 100 ? 0: completed + 1});
   }
 
   render() {
@@ -58,7 +71,13 @@ class App extends Component {
           <TableBody>
             {this.state.customers ? this.state.customers.map(c => { 
               return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.name}gender={c.gender} job={c.job} />);
-            }) : ""}
+            }) : 
+            <TableRow>
+              <TableCell colSpan="6" align="center">
+                <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+              </TableCell>
+            </TableRow>
+            }
           </TableBody>
         </Table>
         </Paper>
